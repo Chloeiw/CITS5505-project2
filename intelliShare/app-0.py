fccf3de10bb572103d6acc647c18797bd687d809
Yifan Gao <gyf971333964@gmail.com>
2024-05-16 16:17:14 +0800

    Merge questionDetails and Profile in 1 app.py

---
+++ intelliShare/app-0.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from db import db
+from models import User
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///IntelliShare.db"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+
+from routes import *
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3000)
