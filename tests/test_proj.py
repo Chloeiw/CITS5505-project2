ea96be13508984328599b19331647479a49a74f9
chloe5yy <117055665+Chloeiw@users.noreply.github.com>
2024-05-19 09:08:55 +0000

    update test

--- tests/test_proj.py
+++ tests/test_proj.py
@@ -1,7 +1,7 @@
 import responses 
 import json 
 
-from intelliShare.models import User
+from intelliShare.models import User, Question, Answer
 
 def test_home(client):
     response = client.get("/")
@@ -19,27 +19,20 @@ def test_registration(client, app):
 
 
 def test_question(client, app):
-    response = client.post("/addQuestion_v1", data={"email": "test@test.com", "password": "testpassword"})
-
+    response = client.post("/addQuestion_v1", data={"title": "What is a bird", "subtitle": "what is a peacock?", "question":"give a description what is a bird"})
+    assert response.status_code == 200
     with app.app_context():
-        assert User.query.count() == 1
-        assert User.query.first().email == "test@test.com"
+        assert Question.query.count() == 1
+        assert Question.query.first().title == "What is a bird"
     pass
 
 
 def test_answer(client, app):
-    response = client.post("/answer", data={"email": "test@test.com", "password": "testpassword"})
-
+    response = client.post("/questionDetails_v1.html", data={"question_id": 1, "answer": "here is a answer to question"})
+    assert response.status_code == 200
     with app.app_context():
-        assert User.query.count() == 1
-        assert User.query.first().email == "test@test.com"
+        assert Answer.count() == 1
+        assert Answer.first().comment == "here is a answer to question"
     pass
 
 
-# def test_search(client, app):
-#     response = client.post("/search", data={"email": "test@test.com", "password": "testpassword"})
-
-#     with app.app_context():
-#         assert User.query.count() == 1
-#         assert User.query.first().email == "test@test.com"
-#     pass
